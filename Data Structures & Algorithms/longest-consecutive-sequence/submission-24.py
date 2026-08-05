93a1726ed470c3db0aa5ecc7e9ec69b1cd2057d1
Mike <110837765+JMikeRivera@users.noreply.github.com>
2026-08-05 01:35:56 -0600

    Add: longest-consecutive-sequence - submission-24

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-24.py
@@ -0,0 +1,56 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        """
+        # empty list edge case
+        if len(nums) == 0:
+            return 0
+        
+        found = {}
+        start = []
+        longest = []
+        res = 0
+        
+        # populate hasmap
+        for i in range(len(nums)):
+            if nums[i] in found:
+                found[nums[i]] += 1
+            else:
+                found[nums[i]] = 1
+        
+        print(found)
+
+        for i in range(len(nums)):
+            if (nums[i]) - 1 not in found:
+                start.append(nums[i])
+
+        print(start)
+
+        i = 0
+        while i < len(start):
+            if start[i] + 1 in found:
+                start[i] += 1
+                longest.append(start[i])
+                if len(longest) > res:
+                    res += 1
+            else:
+                i += 1
+                longest.clear()
+
+        return res + 1
+        """
+        found = set(nums)
+        longest = 0
+
+        for num in found:
+            # num is the beginning of a sequence
+            if num - 1 not in found:
+                current = num
+                current_length = 1
+
+                while current + 1 in found:
+                    current += 1
+                    current_length += 1
+
+                longest = max(longest, current_length)
+
+        return longest
